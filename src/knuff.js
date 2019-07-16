1b00a1da5119edcd59ce877ed9bf092d1e24945a
Oscar Palmér <opalmer@me.com>
2019-07-16 15:45:24 +0200

    0.7.0
    + refactoring
    + minification with terser

--- src/knuff.js
+++ src/knuff.js
@@ -1,158 +1,189 @@
-((scope) => {
-  //  Document for 'scope'
-  const doc = scope.document;
+/*!
+ * Knuff, v0.7.0 - pushstate and popstate-events, but without the "hassle"!
+ * https://github.com/oscarpalmer/knuff
+ * (c) Oscar Palmér, 2019, MIT @license
+ */
+// eslint-disable-next-line no-unused-vars
+const knuff = (function knuff() {
+  // Reference to document window
+  const win = window;
+  // Document object for document window
+  const doc = win.document;
+  // Initial base URL for document window
+  const baseUrl = win.location.href;
 
-  //  Base URL for 'scope'
-  const baseUrl = scope.location.href;
-
-  //  Reference for smarter Array, Element, and Object functions
-  const arrayPrototype = Array.prototype;
+  // Reference for smarter Element and Object functions
   const elementPrototype = Element.prototype;
   const objectPrototype = Object.prototype;
 
-  //  Default options for Knuff
-  const options = {
-    baseUrl: scope.location.protocol + '//' + scope.location.host + '/',
-    selector: '[data-knuff]',
-    onPop: null,
-    onPush: null,
-  };
-
-  /**
-   *  Get the absolute location for a reference string.
-   *
-   *  @param  {String} string - Reference string
-   *  @return {String}          An absolute location reference
-   */
-  const getLocation = (string) => {
-    //  Reference is already absolute
-    if (string.substr(0, options.baseUrl.length) === options.baseUrl) {
-      return string;
-    }
-
-    //  Prepend the base url to the string
-    return options.baseUrl.replace(/\/*$/, '/') + string.replace(/^\/*/, '');
-  };
-
-  /**
-   *  Event handler for 'popstate' events.
-   *
-   *  @param {Event} event - A popstate event
-   */
-  const popListener = (event) => {
-    if (event.state && knuffOnPop) {
-      //  The state property and a user defined callback exists
-      options.onPop.call(event, event, event.state.knuff);
-    }
-  };
-
-  /**
-   *  Event handler for 'pushState' events.
-   *
-   *  @param {Event} event - pushState event
-   */
-  const pushListener = (event) => {
-    //  Easy reference to event target
-    const { target } = event;
-
-    //  The target doesn't match the user defined selector,
-    //  or it does, but the click was supplemented by a modifier key,
-    //  so let's ignore it
-    if (target.matches(options.selector) === false
-        || event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
-      return;
-    }
-
-    //  Get the location for the push
-    let location = target.getAttribute('data-knuff') || target.href || false;
-
-    //  Don't do anything if the location is the same, or if none is available
-    if (location === scope.location.href || location === false) {
-      return;
-    }
-
-    //  The event matches our criteria, so let's cancel built-in events
-    event.preventDefault();
-
-    //  Get the absolute version of the location
-    location = getLocation(location);
-
-    //  Push the location to the browser's history
-    scope.history.pushState({ knuff: location }, null, location);
-
-    //  Call the user defined callback if it exists
-    if (knuffOnPush) {
-      options.onPush.call(event, event, location);
-    }
-  };
-
-  /**
-   *  Override the default options with ones supplied by the user.
-   *
-   *  @param {Object} object - An options object
-   */
-  const setOptions = (object) => {
-    //  'object' isn't a regular object, so no options are overridden
-    if (objectPrototype.toString.call(object) !== '[object Object]') {
-      return;
-    }
-
-    //  Override each default option where possible
-    arrayPrototype.forEach.call(Object.keys(object), (key) => {
-      options[key] = object[key];
-    });
-
-    //  Handy reference for calling the 'popstate' handler
-    if (typeof options.onPop === 'function') {
-      knuffOnPop = true;
-    }
-
-    //  Handy reference for calling the 'pushState' handler
-    if (typeof options.onPush === 'function') {
-      knuffOnPush = true;
-    }
-  };
-
-  //  Knuff is not active, by default
-  let knuffActive = false;
-
-  //  Knuff does not have a 'popstate' handler, by default
-  let knuffOnPop = false;
-
-  //  Knuff does not have a 'pushState' handler, by default
-  let knuffOnPush = false;
-
-  //  Tiny polyfill for the 'matches' method
-  if (typeof elementPrototype.matches === 'undefined') {
+  // Tiny polyfill for the 'matches'-method
+  if (elementPrototype.matches == null) {
     elementPrototype.matches = elementPrototype.msMatchesSelector
                                || elementPrototype.webkitMatchesSelector;
   }
 
   /**
-   *  Global method to activate Knuff.
-   *
-   *  @param {Object} userOptions - Object of options
+   * State variables and options for Knuff.
    */
-  scope.knuff = (userOptions) => {
-    //  If Knuff is already active,
-    //  it should not be possible to active it again
-    if (knuffActive) {
+  const Knuff = {
+    // Does a popstate-callback exist?
+    hasPop: false,
+    // Does a pushstate-callback exist?
+    hasPush: false,
+    // Is Knuff active?
+    isActive: false,
+    options: {
+      // Base URL for Knuff
+      baseUrl: `${win.location.protocol}//${win.location.host}/`,
+      // Base selector for Knuff
+      selector: '[data-knuff]',
+      // No popstate-callback by default
+      onPop: null,
+      // No pushstate-callback by default
+      onPush: null,
+    },
+  };
+
+  /**
+   * Collection of utility methods.
+   */
+  const Utils = {
+    /**
+     * Get the absolute location for a reference string.
+     * @param {String} string Reference string
+     * @return {String} An absolute location reference
+     */
+    getLocation(location) {
+      // Reference is already absolute
+      if (location.substr(0, Knuff.options.baseUrl.length) === Knuff.options.baseUrl) {
+        return location;
+      }
+
+      // Sanitise the base URL
+      const base = Knuff.options.baseUrl.replace(/\/*$/, '/');
+      // Sanitise the path
+      const path = location.replace(/^\/*/, '');
+
+      // Concatenate and return the base URL and the path
+      return `${base}${path}`;
+    },
+
+    /**
+     * Override the default options with ones supplied by the user.
+     * @param {Object} options An options object
+     */
+    setOptions(options) {
+      // The object is bad, or not an actual object, so let's skip it
+      if (options == null
+          || objectPrototype.toString.call(options) !== '[object Object]') {
+        return;
+      }
+
+      // For all items in the object…
+      Object.keys(options).forEach((key) => {
+        // … overwrite matching values in Knuff's options
+        Knuff.options[key] = options[key];
+      });
+
+      // If a callback was added for 'popstate'-calls…
+      if (options.onPop != null
+          && typeof options.onPop === 'function') {
+        // … set its existence variable to true
+        Knuff.hasPop = true;
+      }
+
+      // If a callback was added for 'pushstate'-calls…
+      if (options.onPush != null
+          && typeof options.onPush === 'function') {
+        // … set its existence variable to true
+        Knuff.hasPush = true;
+      }
+    },
+  };
+
+  /**
+   * Collection of event methods.
+   */
+  const Events = {
+    /**
+     * Event handler for 'popstate' events.
+     * @param {Event} event A popstate event
+     */
+    onPop(event) {
+      // The state property and a user defined callback exists…
+      if (event.state && Knuff.hasPop) {
+        // … so let's call it with the correct parameters
+        Knuff.options.onPop.call(event, event, event.state.knuff);
+      }
+    },
+    /**
+     * Event handler for 'pushstate' events.
+     * @param {Event} event A pushstate event
+     */
+    onPush(event) {
+      // Get the event target
+      const { target } = event;
+
+      // The target doesn't match the user defined selector,
+      // or it does, but the click was supplemented by a modifier key,
+      // so let's ignore it
+      if (target.matches(Knuff.options.selector) === false
+          || event.altKey
+          || event.ctrlKey
+          || event.metaKey
+          || event.shiftKey) {
+        return;
+      }
+
+      // Get the location for the push
+      let location = target.getAttribute('data-knuff') || target.href || false;
+
+      // Don't do anything if the location is the same, or if none is available
+      if (location === win.location.href
+          || location === false) {
+        return;
+      }
+
+      // The event matches our criteria, so let's cancel built-in events
+      event.preventDefault();
+
+      // Get the absolute version of the location
+      location = Utils.getLocation(location);
+
+      // Push the location to the browser's history
+      win.history.pushState({ knuff: location }, null, location);
+
+      // Call the user defined callback if it exists
+      if (Knuff.hasPush) {
+        Knuff.options.onPush.call(event, event, location);
+      }
+    },
+  };
+
+  /**
+   * Method for initialising Knuff.
+   * @param {Object} options Object of options
+   */
+  return (options) => {
+    // If Knuff is already active,
+    // it should not be possible to active it again
+    if (Knuff.isActive) {
       return;
     }
 
-    //  Set Knuff to be active
-    knuffActive = true;
+    // Set Knuff to be active
+    Knuff.isActive = true;
 
-    //  Override the default options
-    setOptions(userOptions);
+    // Override the default options
+    Utils.setOptions(options);
 
-    //  Add Knuff's 'popstate' handler to the 'scope'
-    scope.addEventListener('popstate', popListener);
+    // Add Knuff's 'popstate' handler to the 'scope'
+    win.addEventListener('popstate', Events.onPop);
 
-    //  Add knuff's 'pushState' handler to the document of 'scope'
-    doc.addEventListener('click', pushListener);
+    // Add knuff's 'pushstate' handler to the document of 'scope'
+    doc.addEventListener('click', Events.onPush);
 
-    //  Define the original URL in the browser's history
-    scope.history.replaceState({ knuff: baseUrl }, null, baseUrl);
+    // Define the original URL in the browser's history
+    win.history.replaceState({ knuff: baseUrl }, null, baseUrl);
   };
-})(window);
+}());
